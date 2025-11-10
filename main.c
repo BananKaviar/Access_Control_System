90c8c14192ad40c39d1a1cb9aa59e3dd146daea9
Banankaviar <n.jaisu@gmail.com>
2025-11-10 16:27:58 +0100

    before trying remove

--- main.c
+++ main.c
@@ -9,16 +9,17 @@
 
 typedef struct {
     int cardNumber;
-    char creationDate[20];
+    int creationDate;
     int start;
     int end;  //I am creating a struct to assign the card with necessary information
 }Card;
 
 
+
 void createNew(Card *newCard){
     printf("Create a new card: \n");
 
-    GetInput("Enter Creation date in the format (YYMMDD):  \n", newCard->creationDate,20);
+    GetInputInt("Enter Creation date in the format (YYMMDD): ", &newCard->creationDate);  
     
     GetInputInt("Enter Card number:",&newCard->cardNumber);
 
@@ -27,10 +28,21 @@ void createNew(Card *newCard){
     GetInputInt("Enter end day: ",&newCard ->end);
 }
 
+
+
 typedef struct{
     Card *allCards; // this struct is more for listing all cards it stores and keeps a count on them
     int count;
 } CARDSLIST;
+
+int findCard(Card listOfCards[], int totalCards, int cardNumber) {
+    for (int i = 0; i < totalCards; i++) {
+        if (listOfCards[i].cardNumber == cardNumber) {
+            return i; // Found
+        }
+    }
+    return -1; // Not found
+}
 
 int main(){
     CARDSLIST listOfCards;
@@ -54,11 +66,6 @@ int main(){
 
         switch(selection){
             case 3:
-            printf("Type 1. to add and 2. to remove access:");
-            int choice;
-            scanf(" %d", &choice);
-
-                if (choice == 1) {
 
                 if (listOfCards.count == 0) {
                     listOfCards.allCards = malloc(sizeof(Card));
@@ -66,26 +73,39 @@ int main(){
                     int newSize = (listOfCards.count + 1) * sizeof(Card);
                     listOfCards.allCards = realloc(listOfCards.allCards,newSize);
                 }
+                
+                int index = findCard();
+                if index
 
-                 createNew(&listOfCards.allCards[listOfCards.count]);
+                
+
+            
+
+                createNew(&listOfCards.allCards[listOfCards.count]);
+
                 listOfCards.count++ ;
+
                 printf("The employee count:%d\n", listOfCards.count);
 
+            break;
+
+
             // } else if (choice == 2) {
-            //     printf ("Enter Card number to be removed: ");
-            //     int searchID;
-            //     scanf(" %d", &searchID);
-            //     int foundIndex = -1;
+        //         printf ("Enter Card number to be removed: ");
+        //         int searchID;
+        //         scanf(" %d", &searchID);
+        //         int foundId = -1;
 
-            //     int size= listOfCards.count;
+        //         int Cardsize= listOfCards.count;
 
-            // for (int i= 0; i<size; i++){
-            //     if(listOfCards.allCards->cardNumber == searchID){
-            //         foundIndex = i;
-            //         break;
+        //     for (int i= 0; i<Cardsize; i++){
+        //         if(listOfCards.allCards->cardNumber == searchID){
+        //             foundId = i;
+        //             break;
 
-            //     }
-            // }
+        //         }
+        //     }
+        // }
 
             //     int removeCard = (listOfCards.count - 1)* sizeof(Card);
             //     listOfCards.allCards= realloc(listOfCards.allCards, removeCard);
@@ -115,8 +135,10 @@ int main(){
 
                 for(int i = 0; i < listOfCards.count;i++){
                     printf("Number: %d\n", listOfCards.allCards[i].cardNumber);
-                    printf("Creation date: %s\n", listOfCards.allCards[i].creationDate);
+                    printf("Creation date: %d\n", listOfCards.allCards[i].creationDate);
                 }
+
+             printf("The employee count:%d\n", listOfCards.count);
 
                 break;
 
@@ -144,7 +166,7 @@ int main(){
             }
 
             if (foundIndex != -1){
-                printf("Found card id in system! created on %s\n", listOfCards.allCards->creationDate);
+                printf("Found card id in system! created on %d\n", listOfCards.allCards->creationDate);
                 
             } else {
                 printf("Employee with cardnumber %d not found.\n", searchId);
@@ -158,6 +180,7 @@ int main(){
 
     return 0;
 }
+
 
 // function that takes an id and returns true if the id is in the system
 
